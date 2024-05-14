9a7bba24803db8825e5233f279fc54e0e0c6226e
CNCodes <colindavid.nelson@gmail.com>
2024-05-14 12:29:28 -0700

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
               { image: "https://www.compass.com/m/23cef20adc977b0ce9a21e729b55e85f3b68b4ad_img_0_96aa6/origin.jpg", speed: 50},
             ]}
           >
-            <div style={{position:"absolute", top:"25%", left:"33%"}}>
+            <div style={{position:"absolute",marginTop:"33%",marginLeft:"25%"}} class="sm:w-full">
               <h1 className="text-3xl text-white font-extrabold  bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  lg:text-5xl" style={{fontWeight:"bolder"}}>Sonoran Sunset Tile</h1>
               <br/> 
               <p className="text-xl text-white bg-clip-text bg-gradient-to-b from-purple-800 to-purple-600 text-center" style={{fontWeight:"bolder"}}>High Quality Installations in the Desert for over 25 Years.</p>
@@ -49,11 +49,11 @@ const Home = () => {
           <br/>
           <div className="h-px mx-32 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500"></div><br/>
           <div className="w-11/12 mx-auto md:ml-24 lg:columns-2 sm:columns-1 sm:mb-15 justify-center">
-          <h1 className="text-3xl lg:text-5xl text-left font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" style={{fontWeight:"bolder"}}>A Cut Above the Rest</h1>
-              <p className="md:w-3/4 text-transparent text-left bg-clip-text bg-gradient-to-b from-indigo-800 via-purple-600 via-purple-500 to-orange-500" style={{fontWeight:"bolder"}}>Not all tile is set the same. And not all tile companies work the same. At Sonoran Sunset Tile, we strive to bring the best service possible to your home. Whether it’s complicated designs, speed of work, or quality of the final product, at Sonoran Sunset Tile we focus on making your experience the best possible. </p>
+          <h1 className="text-3xl text-center lg:text-5xl lg:text-left font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" style={{fontWeight:"bolder"}}>A Cut Above the Rest</h1>
+              <p className="md:w-3/4 text-center text-transparent lg:text-left bg-clip-text bg-gradient-to-b from-indigo-800 via-purple-600 via-purple-500 to-orange-500" style={{fontWeight:"bolder"}}>Not all tile is set the same. And not all tile companies work the same. At Sonoran Sunset Tile, we strive to bring the best service possible to your home. Whether it’s complicated designs, speed of work, or quality of the final product, at Sonoran Sunset Tile we focus on making your experience the best possible. </p>
               
-          <h1 className="text-3xl lg:text-5xl text-left font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" style={{fontWeight:"bolder"}}>Over 35 Years of Experience</h1>
-              <p className="md:w-3/4 text-transparent text-left bg-clip-text bg-gradient-to-b from-indigo-800 via-purple-600 via-purple-500 to-orange-500" style={{fontWeight:"bolder"}}>Founded in 1994, At Sonoran Sunset Tile, We are proud to share our over 25 years of expertese so you can make the most informed decision when your are deciding on flooring. Whether it is a new home, or remodeling your current residence. We are here to help. </p>
+          <h1 className="text-3xl text-center lg:text-5xl lg:text-left font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" style={{fontWeight:"bolder"}}>Over 35 Years of Experience</h1>
+              <p className="md:w-3/4 text-center text-transparent lg:text-left bg-clip-text bg-gradient-to-b from-indigo-800 via-purple-600 via-purple-500 to-orange-500" style={{fontWeight:"bolder"}}>Founded in 1994, At Sonoran Sunset Tile, We are proud to share our over 25 years of expertese so you can make the most informed decision when your are deciding on flooring. Whether it is a new home, or remodeling your current residence. We are here to help. </p>
               </div>
         <br/>
         <div className="h-px mx-32 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
